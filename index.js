3b9a9a11a7872f7b1cd84546f56545c4baef6d5c
Aaron L <aloken128@gmail.com>
2022-10-23 22:37:57 -0600

    Add Thumbnails and gamemode

--- index.js
+++ index.js
@@ -2,43 +2,44 @@ const dot = require('dotenv').config()
 if (dot.error) throw dot.error
 
 const Discord = require('discord.js')
-let hook
+const Webhook = new Discord.WebhookClient({
+  id: process.env.DISCORD_ID,
+  token: process.env.DISCORD_TOKEN
+})
+
+const GameModes = require('./game_modes')
 
 // Stuff and Things
 const got = require('got')
 const storage = require('node-persist')
+const schedule = require('node-schedule');
 
-// Luxon
-const { DateTime, Duration, Settings } = require('luxon')
-Settings.defaultZoneName = 'America/Denver'
+// Day.js
+const dayjs = require('dayjs')
+const utc = require('dayjs/plugin/utc')
+const timezone = require('dayjs/plugin/timezone')
+const localizedFormat = require('dayjs/plugin/localizedFormat')
+const duration = require('dayjs/plugin/duration')
+
+dayjs.extend(utc)
+dayjs.extend(localizedFormat)
+dayjs.extend(duration)
+dayjs.extend(timezone)
+dayjs.tz.setDefault('America/Denver')
 
 const TEAMS = {
   RADIANT: 0,
   DIRE: 1
 }
 
-const convertTo32 = id => Number(BigInt(id) - BigInt('76561197960265728'))
+let maxLogs = 0
+
+const RealSteamId = Number(BigInt(process.env.STEAMID) - BigInt('76561197960265728'))
 
 const OpenDotaAPI = got.extend({
   prefixUrl: 'https://api.opendota.com/api',
   responseType: 'json'
 })
-
-// Unused at this time...
-const fetchSteamIds = async urlname => {
-  try {
-    const response = await got(`http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/`, {
-      searchParams: {
-        key: process.env.STEAM_API_KEY,
-        vanityurl: urlname
-      },
-      responseType: 'json'
-    })
-    return response.body.response.steamid
-  } catch (error) {
-    throw new Error(error)
-  }
-}
 
 const API_ENDPOINT = id => ({
   players: `players/${id}`,
@@ -61,8 +62,10 @@ const fetchAPI = async (path, id) => {
 const sendDiscordWebhook = ({
   profile,
   match_id,
+  game_mode,
   Win,
   hero,
+  heroStats,
   kills,
   deaths,
   assists,
@@ -78,6 +81,8 @@ const sendDiscordWebhook = ({
   radiant_score,
   Team
 }) => {
+  const heroImg = heroStats.find(x => x.id === hero.id).img || null
+
   const payload = {
     username: `Dota Tracker (v3)`,
     // avatarURL: 'https://avatarfiles.alphacoders.com/372/37238.png',
@@ -92,10 +97,10 @@ const sendDiscordWebhook = ({
         title: `Match #${match_id}`,
         url: `https://www.opendota.com/matches/${match_id}`,
         thumbnail: {
-          url: `https://api.opendota.com${hero.img}`
+          url: `https://api.opendota.com${heroImg}`
         },
         footer: {
-          text: DateTime.now().toLocaleString(DateTime.DATETIME_FULL)
+          text: dayjs.tz().format('LLL')
         },
         description: `**${Win ? 'Win' : 'Loss'}** - __**${
           Team === TEAMS.RADIANT
@@ -109,7 +114,8 @@ const sendDiscordWebhook = ({
               `Hero: ${hero.localized_name}\n` +
               `K/D/A: ${kills}/${deaths}/${assists}\n` +
               `XPM: ${xp_per_min}\n` +
-              `GPM: ${gold_per_min}`,
+              `GPM: ${gold_per_min}\n` +
+              `${GameModes[game_mode].name}`,
             inline: true
           },
           {
@@ -124,14 +130,14 @@ const sendDiscordWebhook = ({
           {
             name: 'Match Details',
             value:
-              `Duration: ${Duration.fromMillis(duration * 1000).toFormat('hh:mm:ss')}\n` +
-              `Start: ${DateTime.fromSeconds(start_time).toLocaleString(DateTime.TIME_SIMPLE)}`
+              `Duration: ${dayjs.duration(duration, 's').format('HH:mm:ss')}\n` +
+              `Start: ${dayjs.unix(start_time).format('LLL')}`
           }
         ]
       }
     ]
   }
-  hook.send(payload)
+  Webhook.send(payload)
 }
 
 const dotaLoop = async id => {
@@ -151,13 +157,50 @@ const dotaLoop = async id => {
     savedGames = games
   }
 
-  // Compair all recent games to see if matchID matches the stored games
+  // Compare all recent games to see if matchID matches the stored games
   const New_Games = games.filter(game => {
     if (savedGames.some(x => x.match_id === game.match_id)) return false
     return true
   })
 
-  if (!New_Games.length) return console.log('No new games... 🙁')
+  // const New_Games = [{
+  //   "match_id": 6718670202,
+  //   "player_slot": 3,
+  //   "radiant_win": false,
+  //   "duration": 1840,
+  //   "game_mode": 22,
+  //   "lobby_type": 0,
+  //   "hero_id": 136,
+  //   "start_time": 1661138202,
+  //   "version": null,
+  //   "kills": 0,
+  //   "deaths": 7,
+  //   "assists": 7,
+  //   "skill": null,
+  //   "average_rank": 43,
+  //   "xp_per_min": 288,
+  //   "gold_per_min": 174,
+  //   "hero_damage": 3688,
+  //   "tower_damage": 0,
+  //   "hero_healing": 129,
+  //   "last_hits": 11,
+  //   "lane": null,
+  //   "lane_role": null,
+  //   "is_roaming": null,
+  //   "cluster": 122,
+  //   "leaver_status": 0,
+  //   "party_size": 4
+  // }]
+
+  if (!New_Games.length) {
+    if (maxLogs >= 50) {
+      return
+    } else {
+      maxLogs += 1
+    }
+    console.log(`No new games... Max Logs ${maxLogs} `)
+    return
+  } 
   /*
    * Extracting infomation since the openapi doesn't include it in recent games
    * Such as score, hero name, hero img, gametype and lobbytype
@@ -166,8 +209,9 @@ const dotaLoop = async id => {
 
   // If new game - get HerosStats
   const [heros, error2] = await fetchAPI('heroes')
+  const [heroStats, heroStatError] = await fetchAPI('heroStats')
   const [profile, error3] = await fetchAPI('players', id)
-  if (error2 || error3) return console.log('Error attempting to fetch heroes/players')
+  if (error2 || error3 || heroStatError) return console.log('Error attempting to fetch heroes/players')
 
   for await (const game of New_Games) {
     const [{ dire_score, radiant_score }, error4] = await fetchAPI('matches', game.match_id)
@@ -185,6 +229,7 @@ const dotaLoop = async id => {
     sendDiscordWebhook({
       ...game,
       hero,
+      heroStats,
       profile,
       Team,
       Win,
@@ -198,14 +243,12 @@ const dotaLoop = async id => {
   console.log('Completed loop. Games saved.')
 }
 
+storage.init()
+
 const init = async () => {
-  const id = convertTo32(process.env.STEAMID)
   try {
-    hook = new Discord.WebhookClient({id: process.env.DISCORD_ID, token: process.env.DISCORD_TOKEN})
-    await storage.init()
-    await dotaLoop(id)
-    hook.destroy()
-    console.log('Completed Script ✅')
+    console.log('starting')
+    await dotaLoop(RealSteamId)
   } catch (error) {
     console.error('Error starting dotaLoop()')
     console.error(error)
@@ -213,3 +256,7 @@ const init = async () => {
 }
 
 init()
+
+const job = schedule.scheduleJob('* * * * *', () => {
+  init()
+})
